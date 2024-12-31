8c1ac685b4ba0a17a6f928ad38701572b43a8541
RicoP <RicoP@users.noreply.github.com>
2024-12-31 02:56:38 +0100

    use minicatch

--- tests/ini.cpp
+++ tests/ini.cpp
@@ -1,10 +1,9 @@
+#include "minicatch.hpp"
 #include <rose/ini.hpp>
 
 #include <string>
 
-#define REQUIRE( COND ) do { if(COND) break; puts("ERROR: " #COND); return -1; } while(0)
-
-int main() {
+TEST_CASE( "basics", "[ini.hpp]" ) {
     // text must be a non-const zero terminated char array.
     char text[] = R"___(
         date = 26th of march 2017
@@ -32,12 +31,9 @@ int main() {
         INI_DOUBLE(ImageData, pixelsum) pixelsum = value;
     INI_END();
 
-    REQUIRE( date     == "26th of march 2017" );
+    REQUIRE( date     == "26th of March 2017" );
     REQUIRE( image    == "splat1.dds" );
     REQUIRE( width    == 1024 );
     REQUIRE( height   == 1024 );
     REQUIRE( pixelsum == 269479.5 );
-
-    puts("<ini.hpp test OK>");
-    return 0;
 }
