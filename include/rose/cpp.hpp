4f19ed7fbc5cd646e465d7351c314dae8f19aeb6
RicoP <RicoP@users.noreply.github.com>
2025-01-25 12:15:03 +0100

    parse simpel struct

--- include/rose/cpp.hpp
+++ include/rose/cpp.hpp
@@ -1,13 +1,30 @@
 #pragma once
 
+// TODO: implement
+#define check(X) (void)(X)
+
+typedef void (*RCppParserCallback)(struct RCpp &, struct RCppScopeGlobal &/*, ...*/);
+typedef void (*RCppParserStructCallback)(struct RCpp &, struct RCppScopeStruct &/*, ...*/);
+typedef void (*RCppParserStructBodyCallback)(struct RCpp &, struct RCppScopeStructBody &/*, ...*/);
+
 struct RCppSpan {
     const char * p;
     int begin;
-    int count;
+    int end;
+
+    static RCppSpan Empty() {
+        return {"", 0, 0};
+    }
+
+    int length() { return end - begin; }
+
+    char get() { return p[begin]; }
+
+    char operator*() { return get(); }
 
     bool compare(const char * rhs) {
         const char * lhs = p + begin;
-        const char * lhs_end = p + begin + count;
+        const char * lhs_end = p + end;
         for(;;) {
             if(lhs == lhs_end) return *rhs == 0;
             if(*lhs != *rhs) return false;
@@ -18,16 +35,73 @@ struct RCppSpan {
     }
 };
 
+
 struct RCpp {
+    // variables
+    void * user = nullptr;
+    char * p = nullptr;
+    int i = 0;
+    int state = 0;
     // cstr/dstr
     RCpp() = default;
     RCpp(char * text) : p(text) {};
     RCpp(const RCpp &) = delete;
-    // variables
-    void * user = nullptr;
-    char * p = nullptr;
-    int state = 0;
 
+
+    void debug_print_state() {
+        printf("(i: %d) -> %.5s\n", i, p+i);
+    }
+
+
+private:
+    bool is_number(char c)     { return c >= '0' && c <= '9'; }
+    bool is_upper(char c)      { return c >= 'A' && c <= 'Z'; }
+    bool is_lower(char c)      { return c >= 'a' && c <= 'z'; }
+    bool is_alpha(char c)      { return is_upper(c) || is_lower(c); }
+    bool is_alpha_num(char c)  { return is_alpha(c) || is_number(c); }
+    bool is_whitespace(char c) { return c == ' ' || c == '\t' || c == '\r' || c == '\n'; }
+
+    void skip_ws() { while(is_whitespace(p[i])) { i++; } }
+
+    RCppSpan keyword_peek() {
+        skip_ws();
+
+        RCppSpan span { p, i, i };
+        if(is_number(*span)) return span;
+        if(!is_alpha(*span)) return span;
+        while(is_alpha_num(p[span.end])) { span.end++; }
+        return span;
+    }
+
+public:
+
+    void tidy_up_comments() {}
+
+    bool keyword_skip(const char * keyword) {
+        RCppSpan span = keyword_peek();
+        if(span.compare(keyword)) {
+            i += span.length();
+            return true;
+        }
+        return false;
+    }
+
+    RCppSpan keyword_read() {
+        RCppSpan keyword = keyword_peek();
+        i+=keyword.length();
+        return keyword;
+    }
+
+    bool symbol_skip(char symbol) {
+        skip_ws();
+        if(p[i] == symbol) {
+            i++;
+            return true;
+        }
+        return false;
+    }
+
+    void parse(RCppParserCallback callback);
 };
 
 struct RCppScopeGlobal {
@@ -41,6 +115,7 @@ struct RCppScopeGlobal {
 struct RCppScopeStruct {
     void * user = nullptr;
     RCppSpan name;
+    bool is_class;
 };
 
 struct RCppScopeVariable {
@@ -60,10 +135,37 @@ struct RCppScopeStructBody {
     };
 };
 
-typedef void (*RCppParserCallback)(RCpp &, RCppScopeGlobal &/*, ...*/);
-typedef void (*RCppParserStructCallback)(RCpp &, RCppScopeStruct &/*, ...*/);
-typedef void (*RCppParserStructBodyCallback)(RCpp &, RCppScopeStructBody &/*, ...*/);
+void RCpp::parse(RCppParserCallback callback) {
+    // TODO: handle errors
+    #define assume(X) (X)
 
-void rose_cpp_parse(RCpp &, RCppParserCallback) {}
+    int n = 0;
+    state = 0;
+
+    tidy_up_comments();
+    for(;;) {
+        skip_ws();
+
+        if(p[i] == 0) return;
+
+        bool is_struct = false;
+        bool is_class  = false;
+        if((is_struct = keyword_skip("struct")) || (is_class = keyword_skip("class"))) {
+            check(is_struct != is_class);
+            RCppScopeStruct Struct { user };
+            Struct.is_class = is_class;
+            Struct.name = keyword_read();
+
+            assume(symbol_skip(';'));
+
+            //RCppScopeGlobal global;
+            //callback(*this, global);
+        }
+    }
+
+    #undef assume
+}
 int  rose_cpp_parse_struct(RCpp &, RCppParserStructCallback) { return 1; }
 int  rose_cpp_parse_struct_body(RCpp &, RCppParserStructBodyCallback) { return 1; }
+
+#undef check
--- tests/cpp.cpp
+++ tests/cpp.cpp
@@ -14,25 +14,15 @@ void check(bool cond) {
 
 TEST_CASE( "basics1", "[cpp.hpp]" ) {
     char text[] = R"___(
-        struct S {
-            int x;
-        };
+        struct S;
     )___";
     RCpp cpp(text);
 
-    rose_cpp_parse(cpp, +[](RCpp & cpp, RCppScopeGlobal & Global) {
+    cpp.parse([](RCpp & cpp, RCppScopeGlobal & Global) {
         switch(Global.type) {
             break; case RCppScopeGlobal::Type::Struct: {
                 int n = rose_cpp_parse_struct(cpp, +[](RCpp & cpp, RCppScopeStruct & Struct) {
                     check(Struct.name.compare("S"));
-                    int n = rose_cpp_parse_struct_body(cpp, +[](RCpp & cpp, RCppScopeStructBody & StructBody) {
-                        switch(StructBody.type) {
-                            break; case RCppScopeStructBody::Type::Variable:
-                            check(StructBody.Variable.type.compare("int"));
-                            check(StructBody.Variable.name.compare("x"));
-                        }
-                    });
-                    check(n == 1);
                 });
                 check(n == 1);
             }
